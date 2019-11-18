e2f15c882279690440903f50d688035a631eb78f
Paolo <paolo.gunnellini@cern.ch>
2019-11-18 18:02:10 +0100

    get the config of data

---
+++ core/model/get_config_requests_data.py
@@ -0,0 +1,87 @@
+import sys
+import json
+import time
+import httplib
+import os
+from urllib2 import Request, urlopen
+import re, urllib
+
+#to use arguments
+listdata=[]
+for arg in sys.argv[1:]:
+    inputfile=str(arg)
+    listdata.append(inputfile)
+
+if(len(listdata)==0):
+    print "Running default example options"
+    listdata.append(['/SingleMuon/Run2018D-HcalCalIterativePhiSym-28Feb2019-v1/ALCARECO','/SingleMuon/Run2017H-17Nov2017-v1/MINIAOD'])
+
+print listdata
+
+def make_simple_request(url):
+    req = Request(url)
+    return json.loads(urlopen(req).read().decode('utf-8'))
+
+def cmsweb_get(url):
+    headers = {'Content-Type': 'application/json',
+               'Accept': 'application/json'}
+    cert = os.getenv('X509_USER_PROXY')
+    key = os.getenv('X509_USER_PROXY')
+    print('Using certificate and key from %s' % cert)
+    if cert is None:
+        print('No X509_USER_PROXY found! Run "voms-proxy-init -voms cms; export X509_USER_PROXY=$(voms-proxy-info --path)" and try again')
+        return {}
+
+    conn = httplib.HTTPSConnection('cmsweb.cern.ch', cert_file=cert, key_file=key)
+    conn.request("GET", url, headers=headers)
+    response = conn.getresponse()
+    status, data = response.status, response.read()
+    conn.close()
+    jsonfile = json.loads(data)
+    idConfig=jsonfile['ConfigCacheID']
+    try:
+        return idConfig
+    except:
+        print('Error parsing JSON from:\n\n%s' % (data))
+
+def cmsweb_getreq(url):
+    cert = os.getenv('X509_USER_PROXY')
+    key = os.getenv('X509_USER_PROXY')
+    print('Using certificate and key from %s' % cert)
+    if cert is None:
+        print('No X509_USER_PROXY found! Run "voms-proxy-init -voms cms; export X509_USER_PROXY=$(voms-proxy-info --path)" and try again')
+        return {}
+
+    conn = httplib.HTTPSConnection('cmsweb.cern.ch', cert_file=cert, key_file=key)
+    conn.request("GET", url)
+    response = conn.getresponse()
+    status, data = response.status, response.read()
+    conn.close()
+   
+    return data
+
+for dataElement in listdata:
+    workflows = make_simple_request('http://vocms074:5984/requests/_design/_designDoc/_view/outputDatasets?key="%s"&limit=10&skip=0&include_docs=True' % (dataElement))
+    workflows = workflows.get('rows', [])
+    request_transitions = []
+    workflow_name = 'empty'
+    configId = 0
+    for workflow in workflows:
+        if '_' in workflow['value']:
+            workflow_name = workflow['value']
+            print workflow_name
+
+        ConfigCacheID=cmsweb_get('/couchdb/reqmgr_workload_cache/%s' % (workflow_name))
+
+        configReqMgrPage = "/couchdb/reqmgr_config_cache/"+str(ConfigCacheID)+'/configFile'
+
+        item_driver = ''
+
+        for item in cmsweb_getreq(configReqMgrPage).split("\n"):
+            if 'with command line options:' in item:
+                item_driver = item.replace('with command line options:','cmsDriver')
+
+        print dataElement
+        print item_driver
+        
+        break
